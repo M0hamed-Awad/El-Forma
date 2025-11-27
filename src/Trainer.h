cf37b867b72d58bb139dff5f9d97d07579d70d5c
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-11-27 08:18:30 +0200

    update Trainer class, adding base logic.

--- src/Trainer.h
+++ src/Trainer.h
@@ -2,13 +2,56 @@
 #define TRAINER_H
 
 #include "User.h"
+#include "Member.h"
+#include <vector>
 
-class Trainer : public User {
+class Trainer : public User
+{
+    string specialty;
+    vector<Member *> assignedMembers;
+
 public:
-    Trainer(int id, string name) : User(id, name) {}
+    Trainer(string trainerName, string trainerEmail, string trainerPassword, string trainerSpecialty)
+        : User(trainerName, trainerEmail, trainerPassword)
+    {
+        specialty = trainerSpecialty;
+    }
 
-    void displayInfo() override {
-        cout << "[Trainer] ID: " << id << " | Name: " << name << endl;
+    // Getters
+    string getTrainerSpecialty() { return specialty; }
+    vector<Member *> getAssignedMembers() { return assignedMembers; }
+
+    // Setters
+    void setTrainerSpecialty(string trainerSpecialty) { specialty = trainerSpecialty; }
+
+    // Functions
+    void manageAccount(int userId) override
+    {
+        cout << "Trainer " << name << " Profile | ID: " << id << endl;
+        cout << "Specialty: " << specialty << endl;
+    }
+
+    void viewAssignedMembers()
+    {
+        cout << "Trainer " << this->name << " assigned members are:" << endl;
+        for (Member *member : assignedMembers)
+        {
+            cout << "Member ID: " << member->getId() << ", Name: " << member->getName() << endl;
+        }
+    }
+
+    void assignMember(Member *member)
+    {
+        // Each Trainer can have only up to 7 members assigned at a time.
+        if (assignedMembers.size() == 7)
+        {
+            cout << ">> Trainer can only Manage 7 Members at a time!" << endl;
+        }
+        else
+        {
+            assignedMembers.push_back(member);
+            cout << ">> System: " << member->getName() << " assigned to " << this->name << endl;
+        }
     }
 };
 
