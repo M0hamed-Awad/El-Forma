eae75cee9e9b5b269579db7939f3ce974620cb1e
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-11-27 07:17:45 +0200

    Refactor User class

--- src/User.h
+++ src/User.h
@@ -5,21 +5,48 @@
 #include <string>
 using namespace std;
 
-class User {
+class User
+{
 protected:
     int id;
     string name;
+    string email;
+    string password;
+    inline static int nextUserId = 0;
 
 public:
-    User(int id, string name) : id(id), name(name) {}
+    User(string userName, string userEmail, string userPassword)
+        : name(userName), email(userEmail), password(userPassword)
+    {
+        nextUserId++;
+        id = nextUserId;
+    }
 
+    // Getters
     int getId() const { return id; }
     string getName() const { return name; }
+    string getEmail() const { return email; }
 
-    // function to test the inheritance
-    virtual void displayInfo() {
-        cout << "ID: " << id << ", Name: " << name << endl;
+    // functions
+    bool login(string userInputEmail, string userInputPassword)
+    {
+        if (userInputEmail == email && userInputPassword == password)
+        {
+            cout << "Login Successful! Welcome, " << name << "." << endl;
+            return true;
+        }
+        else
+        {
+            cout << "Invalid email or password." << endl;
+            return false;
+        }
     }
+
+    void logout() { cout << "Goodbye, " << name << "!" << endl; }
+
+    virtual void manageAccount(int userId) = 0;
+
+    virtual ~User() {}
 };
 
 #endif
